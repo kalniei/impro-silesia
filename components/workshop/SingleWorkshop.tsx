9f4b7a3507a78fc647b887806a33c137810b00b3
ZarOmi <annakalniei@gmail.com>
2022-10-18 22:44:47 +0200

    updatedVersion

--- components/workshop/SingleWorkshop.tsx
+++ components/workshop/SingleWorkshop.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   CardContent,
   CardHeader,
@@ -46,8 +47,6 @@ const SingleWorkshopPage = (): JSX.Element => {
     }
   };
 
-  console.log(workshopDetails);
-
   useEffect(() => {
     if (!router.isReady) return;
     if (router?.query?.id) {
@@ -62,10 +61,17 @@ const SingleWorkshopPage = (): JSX.Element => {
     <Grid container justifyContent="center" m={2}>
       {workshopDetails && (
         <Card sx={{ width: '80%' }}>
-          <CardHeader title={workshopDetails.name} />
           <CardContent>
             <Grid container>
               <Grid item xs={6}>
+                <Grid item>
+                  <Typography variant="h5" sx={{ fontSize: 20 }}>
+                    <Typography component="span" sx={{ fontWeight: 800, fontSize: 20 }}>
+                      {workshopDetails.name}
+                    </Typography>
+                    -{workshopDetails.day_of_week}
+                  </Typography>
+                </Grid>
                 <Grid container>
                   <Grid item xs={6}>
                     <Grid container alignItems="center">
@@ -73,35 +79,93 @@ const SingleWorkshopPage = (): JSX.Element => {
                         <WatchLaterOutlinedIcon sx={{ fontSize: 50 }} />
                       </Grid>
                       <Grid item ml={1}>
-                        <Typography variant="h5">
+                        <Typography
+                          variant="h6"
+                          sx={{
+                            fontSize: 14,
+                            fontWeight: 700,
+                            borderRadius: 1,
+                            backgroundColor: 'orange'
+                          }}
+                          p={0}
+                        >
                           {workshopDetails.workshop_dates?.length} x {workshopDetails.duration}h
                         </Typography>
                       </Grid>
                     </Grid>
 
-                    <Typography variant="h5">
-                      <CreditCardIcon fontSize="large"></CreditCardIcon>
-                      {workshopDetails.price_sale}/{workshopDetails.price_normal} pln
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                      taniej do {workshopDetails.price_date}
-                    </Typography>
+                    <Grid container alignItems="center">
+                      <Grid item>
+                        <CreditCardIcon sx={{ fontSize: 50, fontWeight: 700 }} />
+                      </Grid>
+                      <Grid item ml={1}>
+                        <Typography
+                          variant="h6"
+                          sx={{ fontSize: 14, fontWeight: 700 }}
+                          pb={0}
+                          pt={0}
+                        >
+                          <Typography
+                            component="span"
+                            sx={{
+                              fontWeight: 700,
+                              fontSize: 14,
+                              borderRadius: 1,
+                              backgroundColor: 'orange'
+                            }}
+                            mr={1}
+                          >
+                            {workshopDetails.price_sale}/{workshopDetails.price_normal}
+                          </Typography>
+                          pln
+                        </Typography>
+                        <Typography
+                          sx={{ mb: 1.5, fontSize: 11, fontWeight: 700 }}
+                          color="text.secondary"
+                          pt={0}
+                        >
+                          taniej do {workshopDetails.price_date}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Grid>
 
                   <Grid item xs={6}>
-                    <Typography variant="h5" component="div">
-                      <CalendarMonthIcon fontSize="large"></CalendarMonthIcon>
-                      {workshopDetails.workshop_dates.map((item, i) => (
-                        <option key={i} value={item}>
-                          {item}
-                        </option>
-                      ))}
+                    <Grid container>
+                      <Grid item>
+                        <CalendarMonthIcon sx={{ fontSize: 50 }} />
+                      </Grid>
+                      <Grid item ml={1} pt={0}>
+                        <Typography variant="h6" sx={{ fontSize: 14, fontWeight: 700 }}>
+                          {workshopDetails.workshop_dates.map((item, i) => (
+                            <option key={i} value={item}>
+                              {item}
+                            </option>
+                          ))}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+
+                  <Grid pt="20" item mt={5} mr={15}>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 500 }}>
+                      - 18 godzin warsztatów
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 500 }}>
+                      - możliwośc odrobienia nieobecnośći z inną grupą
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 500 }}>
+                      - pierwsze zajęcia są niezobowiązujące: jeśli zrezygnujesz z kursu - oddamy Ci
+                      pienądze
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 500 }}>
+                      - certyfikat
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 500 }}>
+                      - dostęp społecznoności Impro Silesia
                     </Typography>
                   </Grid>
                 </Grid>
-                <Typography variant="body2" color="text.secondary">
-                  hhghffffff
-                </Typography>
               </Grid>
             </Grid>
           </CardContent>
